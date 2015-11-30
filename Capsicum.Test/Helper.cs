dde54e015632fa9f8f88ff9a8d49a4fef75af249
Nicola <LambdaSix@users.noreply.github.com>
2015-11-30 17:15:02 +0000

    Add some more tests for Entity
    Skeleton out Pool and Group

---
+++ Capsicum.Test/Helper.cs
@@ -0,0 +1,12 @@
+﻿using Capsicum.Interfaces;
+
+namespace Capsicum.Test {
+    public class Helper {
+         
+    }
+
+    public class FakeComponent : IComponent
+    {
+        public string Name { get; set; }
+    }
+}
